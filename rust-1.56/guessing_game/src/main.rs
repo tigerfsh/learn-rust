5a2f40dca5a02d984fec626a17a19b689c8e8573
付寿海 <shouhai.fu@17zuoye.com>
2022-01-04 19:37:16 +0800

    new guessing_game

---
+++ rust-1.56/guessing_game/src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    print!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Faild to read line");
+
+    print!("You guessed: {}", guess);
+}
