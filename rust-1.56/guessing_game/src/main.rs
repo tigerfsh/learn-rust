6a3b3f12dcc48dab713bfa1d160de97b85f02fd8
付寿海 <shouhai.fu@17zuoye.com>
2022-01-10 20:22:24 +0800

    update 20220110

--- rust-1.56/guessing_game/src/main.rs
+++ rust-1.56/guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    print!("The secret number is {}\n", secret_number);
+
 
     print!("Please input your guess.");
 
