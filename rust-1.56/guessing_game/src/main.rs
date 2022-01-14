9a3909ef0e58c679a44ca337f96c3213cd0a5c02
付寿海 <shouhai.fu@17zuoye.com>
2022-01-14 14:28:26 +0800

    0114

--- rust-1.56/guessing_game/src/main.rs
+++ rust-1.56/guessing_game/src/main.rs
@@ -1,5 +1,24 @@
-use std::io;
+// use std::sync::{Mutex, Arc};
+// use std::thread;
+// fn main() {
+//     let counter = Arc::new(Mutex::new(0));
+//     let mut handles = vec![];
+//     for _ in 0..10 {
+//         let counter = Arc::clone(&counter);
+//         let handle = thread::spawn(move || {
+//             let mut num = counter.lock().unwrap();
+//             *num += 1;
+//         });
+//         handles.push(handle);
+//     }
+//     for handle in handles {
+//         handle.join().unwrap();
+//     }
+//     println!("Result: {}", *counter.lock().unwrap());
+// }
+
 use rand::Rng;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -7,7 +26,6 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     print!("The secret number is {}\n", secret_number);
-
 
     print!("Please input your guess.");
 
