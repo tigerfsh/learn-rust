f865c2cb00c190943023f1f794ec5c1dfce8595d
付寿海 <shouhai.fu@17zuoye.com>
2022-01-25 19:43:26 +0800

    update at 20220125

--- rust-1.56/projects/enumes/src/main.rs
+++ rust-1.56/projects/enumes/src/main.rs
@@ -32,25 +32,102 @@
 
 // }
 
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
+// enum IpAddr {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
 
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
 
-impl Message {
-    fn call(&self) {
+// impl Message {
+//     fn call(&self) {
+//     }
+// }
+// fn main() {
+//     let home = IpAddr::V4(127, 0, 0, 1);
+//     let loopback = IpAddr::V6(String::from("::1"));
+//     let m = Message::Write(String::from("hello, world."));
+//     m.call();
+// }
+
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter,
+// }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter => 25,
+//     }
+// }
+// fn main() {
+//     let val = value_in_cents(Coin::Penny);
+//     println!("{}", val);
+// }
+
+
+// #[derive(Debug)] // 这样可以立刻看到州的名称
+// enum UsState {
+//     Alabama,
+//     Alaska,
+//     // --snip--
+// }
+
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(state) => {
+//             println!("State quarter from {:?}!", state);
+//             25
+//         },
+//     }
+// }
+
+// fn main() {
+//     value_in_cents(Coin::Quarter(UsState::Alabama));
+// }
+
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         None => None,
+//         Some(v) => Some(v+1),
+//     }
+// }
+
+// fn main() {
+//     let five = Some(5);
+//     let six = plus_one(five);
+//     let none = plus_one(None);
+//     println!("{:?}, {:?}", six, none);
+// }
+
+fn main() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (), //不做任何处理
     }
 }
-fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
-    let m = Message::Write(String::from("hello, world."));
-    m.call();
-}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
