d08da298b6b1ec4548621bad16f4fe88bce96bb2
付寿海 <shouhai.fu@17zuoye.com>
2022-01-25 18:53:36 +0800

    update at 20220125

---
+++ rust-1.56/projects/enumes/src/main.rs
@@ -0,0 +1,56 @@
+// enum IpAddrKind {
+//     V4,
+//     V6,
+// }
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+// fn main() {
+//     let home = IpAddr {
+//         kind: IpAddrKind::V4,
+//         address: String::from("127.0.0.1"),
+//     };
+
+//     let loopback = IpAddr {
+//         kind: IpAddrKind::V6,
+//         address: String::from("::1"),
+//     };
+
+
+// }
+
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
+// }
+// fn main() {
+//     let home = IpAddr::V4(String::from("127.0.0.1"));
+//     let loopback = IpAddr::V6(String::from("::1"));
+
+// }
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+    }
+}
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("hello, world."));
+    m.call();
+}
