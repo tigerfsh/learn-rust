f391d1199ff050f662af33c9b79245ffcd6124b4
付寿海 <shouhai.fu@17zuoye.com>
2022-02-08 11:46:14 +0800

    update at 20220208

--- rust-1.56/projects/minigrep/src/lib.rs
+++ rust-1.56/projects/minigrep/src/lib.rs
@@ -1,4 +1,18 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
 use std::error::Error;
 use std::fs;
 
@@ -25,7 +39,24 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With contents: {}", contents);
+    // println!("With contents: {}", contents);
+
+    for matched_line in search(&config.query, &contents) {
+        println!("Matched line: {}", matched_line);
+    }
+
     Ok(())
 }
 
+// 当从函数返回一个引用，返回值的生命周期参数需要与一个参数的生命周期参数相匹配
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
